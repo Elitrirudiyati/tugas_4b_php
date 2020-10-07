3b6dbbd3d7fa27dd2fc09218d28bc8eca3ef7dfe
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-10-07 17:25:56 +0700

    Add files via upload

---
+++ tugas_4b.php
@@ -0,0 +1,10 @@
+<?php
+	$saluran = 523;
+	$jumlah_liter = 8891;
+	$rata_rata = ($jumlah_liter/$saluran);
+
+	echo "Jumlah Saluran = $saluran<br>
+		Jumlah Terpakai = $jumlah_liter<br>
+		Rata - rata Pemakaian = ....?<br>
+		Jawaban : $rata_rata liter/keluarga";
+?>
